113b1d51ed75b28dd4f9e34f4603db7830fc6221
Marta Grazia <airwa90@gmail.com>
2022-11-09 11:50:12 +0000

    added testing data attributes

--- js-activities/task 1/mostexpensiveitem.js
+++ js-activities/task 1/mostexpensiveitem.js
@@ -1,22 +1,24 @@
 function mostExpensive (array) {
-    let itemsNumber = array.length;
     let maxPrice = 0;
-    let itemId  
-    for (let i = 0; i < itemsNumber; i++){
+    let itemId;  
+    for (let i = 0; i < array.length; i++){
                 
-        if ( maxPrice < (array[i]["price"] * array[i]["stock"]))
+        if ( maxPrice < (array[i]["price"] * array[i]["stock"])){
            
                 maxPrice = (array[i]["price"] * array[i]["stock"]);
+                console.log(maxPrice);
                 itemId = i;
+                console.log(itemId);
             } 
-        //return array[itemID];     
-        console.log(`The item that's most expensive is: ${(array[itemId]["item"])}, with a total price of: £${maxPrice}`)
+        return array[itemId];     
+        //console.log(`The item that's most expensive is: ${(array[itemId]["item"])}, with a total price of: £${maxPrice}`)
     }
-
+}
 let test = [
-    { item: "irn bru", price: 3.25, stock: 50 },
+    { item: "irn bru", price: 3.25, stock: 50},
     { item: "fanta", price: 3.98, stock: 45 },
     { item: "diet coke", price: 4.40, stock: 38 }, 
     { item: "7up", price: 3.99, stock: 42 }, 
-]
-mostExpensive(test);
+];
+//mostExpensive(test);
+console.log(mostExpensive(test));
